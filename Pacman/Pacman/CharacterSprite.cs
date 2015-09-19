63624ae9ce204edcb05da179679bc7f49b76bfd6
Greg Bunia <greg.bunia@gmail.com>
2015-09-19 03:57:18 +0200

    Pacman

---
+++ Pacman/Pacman/CharacterSprite.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Pacman
+{
+    abstract class CharacterSprite
+    {
+        public Texture2D texture;
+        public Texture2D texture0;
+        public Texture2D texture1;
+        public Vector2 position;
+        public Vector2 size;
+        public int velocity;
+
+        public Vector2 screenSize;
+
+        public Direction direction;
+
+        public CharacterSprite(Texture2D p0, Texture2D p1, Vector2 p, Vector2 s, int screenWidth, int screenHeight, int velocity)
+        {
+            texture = texture0 = p0;
+            texture1 = p1;
+            size = s;
+            screenSize = new Vector2(screenWidth, screenHeight);
+            direction = Direction.Right;
+            position = screenSize / 2;
+            this.velocity = velocity;
+        }
+
+        public void Dispose()
+        {
+            texture0.Dispose();
+            texture1.Dispose();
+        }
+
+        protected Vector2 Movement(Direction direction)
+        {
+            Vector2 movement = new Vector2(0, 0);
+            switch (direction)
+            {
+                case Direction.Right:
+                    movement = new Vector2(velocity, 0);
+                    break;
+                case Direction.Left:
+                    movement = new Vector2(-velocity, 0);
+                    break;
+                case Direction.Down:
+                    movement = new Vector2(0, velocity);
+                    break;
+                case Direction.Up:
+                    movement = new Vector2(0, -velocity);
+                    break;
+            }
+            return movement;
+        }
+
+        public void Move()
+        {
+            position += Movement(direction);
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardState)
+        {
+            Move();
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, float rotation)
+        {
+            if (gameTime.TotalGameTime.Milliseconds % 250 == 0)
+                if (texture == texture1)
+                    texture = texture0;
+                else texture = texture1;
+
+            spriteBatch.Draw(texture, position, null, Color.White, rotation, new Vector2(texture.Width / 2, texture.Height / 2), 1, SpriteEffects.None, 1);
+        }
+
+        protected bool CanMove(Direction direction)
+        {
+            return !(
+                position.X - size.X/2 <= 0 && direction == Direction.Left || position.X + size.X/2 >= screenSize.X && direction == Direction.Right || 
+                position.Y - size.Y/2 <= 0 && direction == Direction.Up   || position.Y + size.Y/2 >= screenSize.Y && direction == Direction.Down
+            );
+        }
+
+
+    }
+}
---
+++ Pacman/Pacman/PacmanSprite.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace Pacman
+{
+    class PacmanSprite : CharacterSprite
+    {
+
+        public Direction inputDirection;
+        private int defaultVelocity;
+        private float rotation;
+
+        public PacmanSprite(Texture2D p0, Texture2D p1, Vector2 p, Vector2 s, int screenWidth, int screenHeight, int velocity) : base(p0, p1, p, s, screenWidth, screenHeight, velocity)
+        {
+            inputDirection = direction;
+            defaultVelocity = velocity;
+            rotation = 0;
+        }
+
+        public static PacmanSprite create(Game game, GraphicsDeviceManager graphics)
+        {
+            PacmanSprite pacmanSprite = new PacmanSprite(game.Content.Load<Texture2D>("Sprites/pacman0"), game.Content.Load<Texture2D>("Sprites/pacman1"), 
+                Vector2.Zero, new Vector2(40f, 40f),
+                graphics.PreferredBackBufferWidth, graphics.PreferredBackBufferHeight, 2);
+            return pacmanSprite;
+        }
+
+        public void Update(GameTime gameTime, KeyboardState keyboardState)
+        {
+            if (keyboardState.IsKeyDown(Keys.Left))
+                inputDirection = Direction.Left;
+            if (keyboardState.IsKeyDown(Keys.Right))
+                inputDirection = Direction.Right;
+            if (keyboardState.IsKeyDown(Keys.Down))
+                inputDirection = Direction.Down;
+            if (keyboardState.IsKeyDown(Keys.Up))
+                inputDirection = Direction.Up;
+
+            if (CanMove(inputDirection))
+                direction = inputDirection;
+            if (CanMove(direction)) {
+                velocity = defaultVelocity;
+               switch (direction)
+                {
+                    case Direction.Right: { rotation = 0; break; }
+                    case Direction.Down:  { rotation = MathHelper.PiOver2; break; }
+                    case Direction.Left:  { rotation = 2 * MathHelper.PiOver2; break; }
+                    case Direction.Up:    { rotation = 3 * MathHelper.PiOver2; break; }
+                }
+            }
+            else
+                velocity = 0;
+
+            base.Update(gameTime, keyboardState);
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch) {
+            base.Draw(gameTime, spriteBatch, rotation);
+        }
+        
+        
+        
+
+    }
+}
